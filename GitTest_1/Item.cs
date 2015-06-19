0a92c79d38ec435f5659ed1c22a3cd4ac3f7dda4
Warotarou <hau-hau@short.biglobe.ne.jp>
2015-06-20 08:31:57 +0900

    add Item.cs

---
+++ GitTest_1/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest_1
+{
+	public class Item
+	{
+		public string Name { get; private set; }
+		public int Value { get; private set; }
+
+		public Item(string name, int value)
+		{
+			if(string.IsNullOrEmpty(name)) throw new ArgumentNullException("name");
+			this.Name = name;
+			this.Value = value;
+		}
+	}
+
+}
